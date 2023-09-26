51666b1b271aab816577429e1824c78c02637c26
EvanBatac <133208566+EvanBatac@users.noreply.github.com>
2023-09-26 13:27:05 +0800

    Array.java

--- Array.java
+++ Array.java
@@ -12,7 +12,17 @@ public class Array {
       arr.RemoveValue(1);
       arr.ViewElements();
       
+      int searchResult = arr.Search(5);
+      if(searchResult != -1 ){
+         System.out.println("The Value is found in index " + searchResult);
+      }
+      else{
+         System.out.println("The Value is not found");
+      }
       
+      arr.Sort();
+      arr.ViewElements();
+
    }
 }
 
@@ -39,6 +49,34 @@ class MyArray {
             System.out.println("Invalid index for removing a value.");
         } 
     }
+    public void Edit(int index, int value){
+      if (index >= 0 && index < size) {
+             array[index] = value;    
+        } else {
+            System.out.println("Invalid index for editing a value.");
+        } 
+    }
+    public int Search(int value){
+      for(int i = 0; i < array.length; i++){
+         if(value == array[i]){
+            System.out.println(i);
+            return i;
+         }  
+      }
+      return -1;
+    }
+      public void Sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    } 
+
     public void ViewElements() {
         System.out.println("Array: " + java.util.Arrays.toString(array));
     }
