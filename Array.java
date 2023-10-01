d8c93328e62d20371d1b03df59489aee61ec0ed6
Evan Batac <133208566+EvanBatac@users.noreply.github.com>
2023-10-01 14:54:20 +0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,129 +1,155 @@
 import java.util.Scanner;
-public class Array {
-    public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
+public class Array2
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Array Size: ");
+        int size = scan.nextInt();
+        
+        MyArray arr = new MyArray(size);
+		
+		int choice = 0;
+		do{
+		    System.out.println("1. Add Element");
+            System.out.println("2. View Elements");
+            System.out.println("3. Remove an Element");
+            System.out.println("4. Search Element");
+            System.out.println("5. Sort Elements");
+            System.out.println("6. Edit Elements");
+            System.out.println("7. Exit");
       
-      System.out.print("Enter Array Size: ");
-      int size = scan.nextInt();
-      
-      MyArray arr = new MyArray(size);
-      int choice = 0;
-      do{
-         System.out.println("1. Add Element");
-         System.out.println("2. View Elements");
-         System.out.println("3. Remove an Element");
-         System.out.println("4. Search Element");
-         System.out.println("5. Sort Elements");
-         System.out.println("6. Edit Elements");
-         System.out.println("7. Exit");
-      
-         System.out.print("Enter Your Choice: ");
-         choice = scan.nextInt();
-      
-         switch(choice){
-            case 1:
-               System.out.print("Enter Array Index: ");
-               int index = scan.nextInt();
-               System.out.print("Enter The Value: ");
-               int value = scan.nextInt();
-               arr.addElement(index, value);
-               break;
+            System.out.print("Enter Your Choice: ");
+            choice = scan.nextInt();
             
-            case 2:
-               arr.ViewElements();
-               break;
+            switch(choice){
+                case 1: 
+                    System.out.print("Enter the Value: ");
+                    int value = scan.nextInt();
+                    arr.AddElement(value);
+                    break;
+                
+                case 2: 
+                    arr.ViewElements();
+                    break;
+                    
+                case 3: 
+                    System.out.print("Enter the index of the value you want to remove: ");
+                    int remove = scan.nextInt();
+                    arr. RemoveElement(remove);
+                    break;
+                    
+                case 4: 
+                    System.out.print("Enter the value that you want to Search: ");
+                    int search = scan.nextInt();
             
-            case 3: 
-               System.out.print("Enter the index of the value you want to remove: ");
-               int remove = scan.nextInt();
-               arr. RemoveElement(remove);
-               break;
+                    int searchResult = arr.Search(search);
             
-            case 4:
-               System.out.print("Enter the value that you want to Search: ");
-               int search = scan.nextInt();
-            
-               int searchResult = arr.Search(search);
-            
-               if(searchResult != -1 ){
-                 System.out.println("The Value is found in index " + searchResult);
-               }
-               else{
-                 System.out.println("The Value is not found");
-               }
-               break;
-            
-            case 5:
-               System.out.println("1. Ascending");
-               System.out.println("2. Descending");
-               System.out.print("Enter Your Choice: ");
-               int ch = scan.nextInt();
-            
-               switch(ch){
-                  case 1:
-                     arr.Sort();
+                    if(searchResult != -1 ){
+                        System.out.println("The Value is found in index " + searchResult);
+                     }
+                     else{
+                           System.out.println("The Value is not found");
+                     }
+                     break;
+                     
+                case 5: 
+                    System.out.println("1. Ascending");
+                    System.out.println("2. Descending");
+                    System.out.print("Enter Your Choice: ");
+                    int ch = scan.nextInt();
+                    
+                    switch(ch){
+                          case 1: 
+                              arr.Asort();
+                              arr.ViewElements();
+                               break;
+                        
+                          case 2: 
+                              arr.Dsort();
+                              arr.ViewElements();
+                              break;    
+                   }
+                   break;
+                   
+                 case 6: 
+                     System.out.print("Enter the Array Index you want to Edit: ");
+                     int edit = scan.nextInt();
+                     System.out.print("Enter The Value: ");
+                     int val = scan.nextInt();
+                     arr.EditElement(edit, val);
                      arr.ViewElements();
-               } 
-          
-            case 6: 
-               System.out.print("Enter the Array Index you want to Edit: ");
-               int edit = scan.nextInt();
-               System.out.print("Enter The Value: ");
-               int val = scan.nextInt();
-               arr.Edit(edit, val);
-               arr.ViewElements();
-               break;
-            
-            case 7:
-               System.out.println("Program Finished!");
-               break;
-      }
-     }
-     while(choice != 7);             
-   }
+                     break;
+                     
+                 case 7: 
+                     System.out.println("Program Finished!");
+
+            }
+
+		}
+		while(choice != 7);
+      
+      scan.close();
+	}
 }
-
-class MyArray {
+class MyArray{
     private int[] array;
-    private int size;
-
-    MyArray(int size) {
+    private int size, index, ctr;
+    MyArray(int size){
         this.size = size;
         array = new int[size];
+        index = 0;
+        ctr = 0;
     }
-
-    public void addElement(int index, int value) {
-        if (index >= 0 && index < size) {
-             array[index] = value;
-        } else {
-            System.out.println("Invalid index for adding a value.");
-        } 
+    public boolean isFull(){
+        return ctr >= size;
     }
-    public void RemoveElement(int index){
-      if (index >= 0 && index < size) {
-             array[index] = 0;
-        } else {
+    public boolean isEmpty(){
+        return ctr == 0;
+    }
+    public void AddElement(int value){
+        if(!isFull()){
+            array[index] = value;
+            index++;
+            ctr++;
+        }
+        else {
+            System.out.println("Array is full!");
+        }
+    }
+    public void ViewElements(){
+        for(int i = 0; i < array.length; i++){
+            System.out.print("[" + array[i] + "]");
+        }
+        System.out.print("Array Element Count: " + ctr);
+        System.out.println();
+        if(isEmpty()){
+            System.out.println("Array is Empty!");
+        }
+    }
+    public void RemoveElement(int removeIndex){
+        if(removeIndex >= 0 && removeIndex < ctr){
+            for (int i = removeIndex; i < ctr - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[ctr - 1] = 0;
+            ctr--;
+            index--;
+        }
+        else{
             System.out.println("Invalid index for removing a value.");
-        } 
+        }
     }
-    public void Edit(int index, int value){
-      if (index >= 0 && index < size) {
-             array[index] = value;    
-        } else {
-            System.out.println("Invalid index for editing a value.");
-        } 
-    }
-    public int Search(int value){
+    public int Search(int SearchValue){
       for(int i = 0; i < array.length; i++){
-         if(value == array[i]){
-            System.out.println(i);
+         if(SearchValue == array[i]){
             return i;
          }  
       }
       return -1;
-    }
-      public void Sort() {
-        for (int i = 0; i < size - 1; i++) {
+   }
+   public void Asort(){
+      for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
@@ -132,8 +158,25 @@ class MyArray {
                 }
             }
         }
-    } 
-    public void ViewElements() {
-        System.out.println("Array: " + java.util.Arrays.toString(array));
-    }
+   }
+   public void Dsort(){
+      for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+   }
+   public void EditElement(int editIndex, int editValue){
+      if (editIndex >= 0 && editIndex < ctr) {
+             array[editIndex] = editValue;    
+        } else {
+            System.out.println("Invalid index for editing a value.");
+        } 
+
+   }
 }
