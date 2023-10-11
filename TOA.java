974506a7d7971ce1a15ee4a92832f99e382590cd
Evan Batac <133208566+EvanBatac@users.noreply.github.com>
2023-10-11 21:33:21 +0800

    Update TOA.java

--- TOA.java
+++ TOA.java
@@ -17,7 +17,16 @@ public class TOA
 		        
 		        double MoalboalDistance = 86.7;
 		        int estHour = (int)MoalboalDistance/speed;
-		        int estMinutes = (int)MoalboalDistance%speed;
+		        int estMinutes = (int)(MoalboalDistance%speed) * 60 / speed;
+              System.out.println("Cebu, City(Emall) - Start");
+              System.out.println("Route 1 - Minglanilla, Cebu");
+              System.out.println("Route 2 - San Fernando, Cebu");
+              System.out.println("Route 3 - Carcar, Cebu");
+              System.out.println("Route 4 - Barili, Cebu");
+              System.out.println("Route 5 - Dumanjug, Cebu");
+              System.out.println("Route 6 - Alcantara, Cebu");
+              System.out.println("End(Moalboal, Cebu)");
+              System.out.println("Speed: " + speed + " km/hr");
               System.out.println("Total Distance: " + MoalboalDistance);
 		        System.out.println("Estimated Time Arrival: " + estHour + " Hour/s " + estMinutes + " Minutes");
               break;
@@ -30,24 +39,43 @@ public class TOA
               
               switch(ch2){
                   case 'N':
-                     double MoalboalDistance2 = 101.4;
+                     double MoalboalDistance2 = 124.7;
                      int estHour2 = (int)MoalboalDistance2/speed;
-		               int estMinutes2 = (int)MoalboalDistance2%speed;
+		               int estMinutes2 = (int)(MoalboalDistance2%speed) * 60 / speed;
+                     System.out.println("Cebu, City(Emall) - Start");
+                     System.out.println("Route 1 - Minglanilla, Cebu");
+                     System.out.println("Route 2 - San Fernando, Cebu");
+                     System.out.println("Route 3 - Carcar, Cebu");
+                     System.out.println("Route 4 - Sibonga, Cebu");
+                     System.out.println("Route 5 - Dumanjug, Cebu");
+                     System.out.println("Route 6 - Alcantara, Cebu");
+                     System.out.println("End(Moalboal, Cebu)");
+                     System.out.println("Speed: " + speed + " km/hr");
                      System.out.println("Total Distance: " + MoalboalDistance2);
 		               System.out.println("Estimated Time Arrival: " + estHour2 + " Hour/s " + estMinutes2 + " Minutes");
                      break;
                      
                    case 'Y':
-                     double MoalboalDistance3 = 96.6;
+                     double MoalboalDistance3 = 131.6;
                      int estHour3 = (int)MoalboalDistance3/speed;
-		               int estMinutes3 = (int)MoalboalDistance3%speed;
+		               int estMinutes3 = (int)(MoalboalDistance3%speed) * 60 / speed;
+                     System.out.println("Cebu, City(Emall) - Start");
+                     System.out.println("Route 1 - Minglanilla, Cebu");
+                     System.out.println("Route 2 - San Fernando, Cebu");
+                     System.out.println("Route 3 - Carcar, Cebu");
+                     System.out.println("Route 4 - Sibonga, Cebu");
+                     System.out.println("Route 5 - Argao, Cebu");
+                     System.out.println("Route 6 - Ronda, Cebu");
+                     System.out.println("Route 7 - Alcantara, Cebu");
+                     System.out.println("End(Moalboal, Cebu)");
+                     System.out.println("Speed: " + speed + "km/hr");
                      System.out.println("Total Distance: " + MoalboalDistance3);
 		               System.out.println("Estimated Time Arrival: " + estHour3 + " Hour/s " + estMinutes3 + " Minutes");
                      break;
 
 
               }
-
+              break;
  		        
 		}
 	}
