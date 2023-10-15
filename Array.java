5a3d7d12414e6892a701dd357c0c1728b4d8a615
Evan Batac <133208566+EvanBatac@users.noreply.github.com>
2023-10-15 20:24:01 +0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,96 +1,139 @@
 import java.util.Scanner;
-public class Array2
-{
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Enter Array Size: ");
-        int size = scan.nextInt();
-        
+public class Array2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int size;
+        do {
+            System.out.print("Enter Array Size: ");
+            try {
+                size = scan.nextInt();
+                if (size <= 0) {
+                    System.out.println("Array size must be a positive integer.");
+                    continue;
+                }
+                break; 
+            } catch (Exception e) {
+                System.out.println("Invalid input for array size. Please enter a valid integer.");
+                scan.nextLine(); 
+              }
+        } while (true);
         MyArray arr = new MyArray(size);
-		
-		int choice = 0;
-		do{
-		    System.out.println("1. Add Element");
+
+        int choice = 0;
+        do {
+            System.out.println("1. Add Element");
             System.out.println("2. View Elements");
             System.out.println("3. Remove an Element");
             System.out.println("4. Search Element");
             System.out.println("5. Sort Elements");
             System.out.println("6. Edit Elements");
             System.out.println("7. Exit");
-      
+
             System.out.print("Enter Your Choice: ");
-            choice = scan.nextInt();
-            
-            switch(choice){
-                case 1: 
+            try {
+                choice = scan.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input for choice. Please enter a valid integer.");
+                scan.nextLine();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
                     System.out.print("Enter the Value: ");
-                    int value = scan.nextInt();
-                    arr.AddElement(value);
+                    try {
+                        int value = scan.nextInt();
+                        arr.AddElement(value);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input for element value. Please enter a valid integer.");
+                        scan.nextLine();
+                    }
                     break;
-                
-                case 2: 
+
+                case 2:
                     arr.ViewElements();
                     break;
-                    
-                case 3: 
+
+                case 3:
                     System.out.print("Enter the index of the value you want to remove: ");
-                    int remove = scan.nextInt();
-                    arr. RemoveElement(remove);
+                    try {
+                        int remove = scan.nextInt();
+                        arr.RemoveElement(remove);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input for removal index. Please enter a valid integer.");
+                        scan.nextLine();
+                    }
                     break;
-                    
-                case 4: 
+
+                case 4:
                     System.out.print("Enter the value that you want to Search: ");
-                    int search = scan.nextInt();
-            
-                    int searchResult = arr.Search(search);
-            
-                    if(searchResult != -1 ){
-                        System.out.println("The Value is found in index " + searchResult);
-                     }
-                     else{
-                           System.out.println("The Value is not found");
-                     }
-                     break;
-                     
-                case 5: 
+                    try {
+                        int search = scan.nextInt();
+                        int searchResult = arr.Search(search);
+
+                        if (searchResult != -1) {
+                            System.out.println("The Value is found in index " + searchResult);
+                        } else {
+                            System.out.println("The Value is not found");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Invalid input for search value. Please enter a valid integer.");
+                        scan.nextLine(); 
+                    }
+                    break;
+
+                case 5:
                     System.out.println("1. Ascending");
                     System.out.println("2. Descending");
                     System.out.print("Enter Your Choice: ");
-                    int ch = scan.nextInt();
-                    
-                    switch(ch){
-                          case 1: 
-                              arr.Asort();
-                              arr.ViewElements();
-                               break;
-                        
-                          case 2: 
-                              arr.Dsort();
-                              arr.ViewElements();
-                              break;    
-                   }
-                   break;
-                   
-                 case 6: 
-                     System.out.print("Enter the Array Index you want to Edit: ");
-                     int edit = scan.nextInt();
-                     System.out.print("Enter The Value: ");
-                     int val = scan.nextInt();
-                     arr.EditElement(edit, val);
-                     arr.ViewElements();
-                     break;
-                     
-                 case 7: 
-                     System.out.println("Program Finished!");
+                    try {
+                        int ch = scan.nextInt();
 
+                        switch (ch) {
+                            case 1:
+                                arr.Asort();
+                                arr.ViewElements();
+                                break;
+
+                            case 2:
+                                arr.Dsort();
+                                arr.ViewElements();
+                                break;
+                            default:
+                                System.out.println("Invalid choice for sorting. Please enter 1 or 2.");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Invalid input for sorting choice. Please enter 1 or 2.");
+                        scan.nextLine(); 
+                    }
+                    break;
+
+                case 6:
+                    System.out.print("Enter the Array Index you want to Edit: ");
+                    try {
+                        int edit = scan.nextInt();
+                        System.out.print("Enter The Value: ");
+                        int val = scan.nextInt();
+                        arr.EditElement(edit, val);
+                    } catch (Exception e) {
+                        System.out.println("Invalid input for edit index or value. Please enter valid integers.");
+                        scan.nextLine();
+                    }
+                    break;
+
+                case 7:
+                    System.out.println("Program Finished!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
             }
 
-		}
-		while(choice != 7);
-      
-      scan.close();
-	}
+        } while (choice != 7);
+
+        scan.close();
+    }
 }
 class MyArray{
     private int[] array;
